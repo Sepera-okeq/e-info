0a1cf30d07683265465224bd767de8bcc676463c
Timur Ismagilov <bouncepaw2@ya.ru>
2024-03-22 09:39:26 +0300

    bababbababab

--- backend/school/urls.py
+++ backend/school/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+# См. https://docs.djangoproject.com/en/5.0/topics/http/urls/
 urlpatterns = [
     path("", views.index, name="index"),
+    path("sonya", views.sonya, name="sonya"),
+    path("account/register", views.account_register, name="account_register")
 ]
--- backend/school/views.py
+++ backend/school/views.py
@@ -1,7 +1,24 @@
+import json
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def sonya(request):
+    return HttpResponse("Соняяяяяя")
+
+@csrf_exempt 
+def account_register(request):
+    if request.method != "POST":
+        return HttpResponse("404 Ты дурак")
+
+    student = json.loads(request.body)
+    # тут регистрируем
+
+    return JsonResponse(student)
