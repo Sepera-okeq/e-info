f7acaf11b0ca9b23b79258274909aa88bdf1d28f
Sepera-okeq <54331959+Sepera-okeq@users.noreply.github.com>
2024-04-05 05:49:44 +0300

    fix: mirror

--- frontend/app/profile/layout.tsx
+++ frontend/app/profile/layout.tsx
@@ -68,7 +68,7 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
             </div>
           </div>
         </div>
-        <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex-1 space-y-4 p-4 pt-6">
           <div className="space-y-6 p-10 pb-16 md:block">
             <div className="space-y-0.5">
               <h2 className="text-2xl font-bold tracking-tight">Настройки</h2>
